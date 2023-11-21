46e575086db9f66163cf96a0a951337b308e4786
kroshkamedved <kroshkamedved@gmail.com>
2023-11-21 21:45:52 +0200

    initial commit from local

---
+++ src/main/java/com/en/abbrevio/AbbrevioApplication.java
@@ -0,0 +1,13 @@
+package com.en.abbrevio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbbrevioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbbrevioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/en/abbrevio/AbbrevioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.en.abbrevio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbbrevioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
