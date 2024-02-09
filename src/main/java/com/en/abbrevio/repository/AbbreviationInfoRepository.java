8bd87f827a7f8cd6ca26d7985dcbb136ab00b854
kroshkamedved <kroshkamedved@gmail.com>
2024-02-09 15:10:32 +0200

    fix: Replace case-sensetive "getBySynonym" query with a case-insensetive one.

--- src/main/java/com/en/abbrevio/repository/AbbreviationInfoRepository.java
+++ src/main/java/com/en/abbrevio/repository/AbbreviationInfoRepository.java
@@ -2,11 +2,13 @@ package com.en.abbrevio.repository;
 
 import com.en.abbrevio.model.AbbreviationInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface AbbreviationInfoRepository extends JpaRepository<AbbreviationInfo, Long> {
+    @Query("SELECT ai FROM AbbreviationInfo ai WHERE UPPER(ai.synonym) = :synonym")
     Optional<AbbreviationInfo> getBySynonym(String synonym);
 }
